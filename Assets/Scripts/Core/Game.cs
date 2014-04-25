24e79f4c47bab676bdb20a6f8921328142a85577
Adam Proctor <adam@uken.com>
2014-04-25 19:14:50 -0400

    startup screen

---
+++ Assets/Scripts/Core/Game.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Game {
+
+  public enum Scenes {
+    Start,
+    Game
+  }
+
+  public static void Initialize() {
+    //Initialize app level stuff here
+  }
+
+  public static void LoadScene(Game.Scenes scene) {
+    Application.LoadLevel(scene.ToString().ToLower());
+  }
+
+}
---
+++ Assets/Scripts/Core/Startup.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Startup : MonoBehaviour {
+
+	void Start () {
+		Game.Initialize ();
+
+    Debug.Log("Welcome Thrillho!");
+
+    Game.LoadScene(Game.Scenes.Game);
+	}
+}
