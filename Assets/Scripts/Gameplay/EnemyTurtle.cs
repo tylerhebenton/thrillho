8515c8e6a44f1f78e4c7cee876132b56c253a222
Tyler Hebenton <tyler@uken.com>
2014-04-26 17:28:44 -0400

    Enemy Stuff + goal placeholder.

---
+++ Assets/Scripts/Gameplay/EnemyTurtle.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyTurtle : MonoBehaviour {
+
+	public GameObject gunObject;
+	public GameObject bullet;
+
+	public void Shoot(){
+		GameObject Bullet;
+		Bullet = Instantiate(bullet, gunObject.transform.position, gunObject.transform.rotation) as GameObject;
+		bullet.GetComponent<Bullet>().velocity = gunObject.transform.TransformDirection(Vector3.up * 10);
+	}
+}
