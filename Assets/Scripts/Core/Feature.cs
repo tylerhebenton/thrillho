86818533b4c2ec9582d28b2dbf0f67a545481be7
Adam Proctor <adam@uken.com>
2014-04-25 22:35:17 -0400

    yolo

--- Assets/Scripts/Core/Feature.cs
+++ Assets/Scripts/Core/Feature.cs
@@ -5,7 +5,7 @@ public static class Feature {
 
   public const string DEBUG_LEVEL_CONTROLS = "DEBUG_LEVEL_CONTROLS";
 
-  private Dictionary<string, bool> featureMap;
+  private static Dictionary<string, bool> featureMap;
 
   static Feature() {
     featureMap = new Dictionary<string, bool>();
