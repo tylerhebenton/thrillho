c7ed43f0d90ff1b3d6213aff33d47d732ed0a4ad
Adam Proctor <adam@uken.com>
2014-04-25 23:03:37 -0400

    kill volume stub

---
+++ Assets/Scripts/Gameplay/KillVolume.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillVolume : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collider){
+		/* Platformer2D reference code
+		baked_EnemyController bec = collider.GetComponent<baked_EnemyController>();
+		if(bec){
+			bec.Redirect(walkDirection);
+		}
+		*/
+	}
+}
