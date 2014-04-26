76b114a578bb0d457588270ff2e1fcb0946409c9
Petar Markovich <petar.markovich@uken.com>
2014-04-25 23:38:56 -0400

    Character Controller

---
+++ Assets/Scripts/Core/InputAxes.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class InputAxes {
+
+	public const string HORIZONTAL = "Horizontal";
+	public const string VERTICAL = "Vertical";
+	public const string JUMP = "Jump";
+	public const string ATTACK_MELEE = "Fire1";
+	public const string ATTACK_RANGED = "Fire2";
+
+}
