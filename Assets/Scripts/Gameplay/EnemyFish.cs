c284b07abeae11ce3761f8f3a1fab55d239db052
Tyler Hebenton <tyler@uken.com>
2014-04-27 17:12:56 -0400

    added LD stuff

---
+++ Assets/Scripts/Gameplay/EnemyFish.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyFish : Monster {
+
+	public GameObject gunObject;
+	public GameObject AnimProxy;
+	public bool move = false;
+	public Vector3 velocity;
+
+  public override void OnAggro() {
+		move = true;
+		Debug.Log ("playinganim on    " + gameObject + "   via Enemyfish.cs");
+		AnimProxy.gameObject.animation.Play ();
+		base.OnAggro();
+    //TODO enable animations
+  }
+
+	void Update(){
+		if(move){
+			this.transform.position = this.transform.position + (Time.deltaTime * velocity);
+		}
+	}
+}
+
