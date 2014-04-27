e3682d78d33e936abbaf2f356a0f58a851916a49
Tyler Hebenton <tyler@uken.com>
2014-04-27 13:11:15 -0400

    LD Stuff

--- Assets/Scripts/Gameplay/EnemyTurtle.cs
+++ Assets/Scripts/Gameplay/EnemyTurtle.cs
@@ -4,17 +4,13 @@ using System.Collections;
 public class EnemyTurtle : Monster {
 
 	public GameObject gunObject;
-	public GameObject bullet;
-
-	public void Shoot(){
-		GameObject Bullet;
-		Bullet = Instantiate(bullet, gunObject.transform.position, gunObject.transform.rotation) as GameObject;
-		bullet.GetComponent<Bullet>().velocity = gunObject.transform.TransformDirection(Vector3.up * 4);
-	}
+	public GameObject prefab;
+	public GameObject AnimProxy;
 
   public override void OnAggro() {
-		this.gameObject.animation.Play ();
-   		base.OnAggro();
+		Debug.Log ("playinganim on turtle");
+		AnimProxy.gameObject.animation.Play ();
+		base.OnAggro();
     //TODO enable animations
   }
 }
